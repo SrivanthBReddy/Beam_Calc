fd5f4edf9532b404bf22bf274e5a55cbfcaef244
SrivanthBReddy <srivanthbojja@gmail.com>
2026-08-01 15:58:55 -0400

    Changed file name to better reflect function

--- beamquestions.py
+++ beamquestions.py
@@ -1,17 +1,17 @@
-import beamcalc
+import beamobjects
 
 beam_type = input("Preset or Custom? ")
 
 if beam_type.lower() == "preset":
     material = input("Wood, Concrete, or Steel?").lower()
-    beaminfo = beamcalc.beam(material)
+    beaminfo = beamobjects.beam(material)
     beamlength = float(input("What is the length of your beam in meters? "))
 elif beam_type.lower() == "custom":
     name = input("Enter your material name: ")
     youngs_modulus = float(input("Enter Young's Modulus in Pascals: "))
     yield_strength = float(input("Enter Yield Strength in Pascals: "))
     density = float(input("Enter Density in kg/m^3: "))
-    beaminfo = beamcalc.beam(material=name, youngs_modulus=youngs_modulus, yield_strength=yield_strength, density=density)
+    beaminfo = beamobjects.beam(material=name, youngs_modulus=youngs_modulus, yield_strength=yield_strength, density=density)
     beamlength = float(input("What is the length of your beam in meters? "))
 else:
     raise ValueError("Invalid beam type. Please enter 'Preset' or 'Custom'.")
@@ -22,7 +22,7 @@ if supports > 0:
     for i in range(supports):
         support_type = input(f"Enter the type of support (Pin, Roller, or Fixed) {i+1}: ")
         distance = float(input(f"Enter the distance of support {i+1} from the left support: "))
-        support = beamcalc.SupportObj(support_type, distance)
+        support = beamobjects.SupportObj(support_type, distance)
         supports_list[i] = support
 else:
     raise ValueError("Invalid number of supports. Must be greater than 0.")
@@ -32,7 +32,7 @@ numpinputs = numinputs.lower()
 
 #Optional Input for point loads
 if numinputs == "none":
-    finalbeam = beamcalc.BeamFinalPlain(beam=beaminfo, length=beamlength, supports=supports_list)   
+    finalbeam = beamobjects.BeamFinalPlain(beam=beaminfo, length=beamlength, supports=supports_list)   
 if numinputs == "point" or numinputs == "both":
     loads = int(input("How many point loads? "))
     if loads > 0:
@@ -41,10 +41,10 @@ if numinputs == "point" or numinputs == "both":
             force = float(input(f"Enter the force of load {i+1} in Newtons: "))
             distance = float(input(f"Enter the distance of load {i+1} from the left support in meters: "))
             point_load_direction = input(f"Enter the direction of load {i+1} (up or down): ")
-            point_load = beamcalc.PointLoad(force, distance, point_load_direction)  
+            point_load = beamobjects.PointLoad(force, distance, point_load_direction)  
             point_loads_list[i] = point_load
         if numinputs == "point":
-            finalbeam = beamcalc.BeamFinalPoint(beam=beaminfo, length=beamlength, supports=supports_list, point_loads=point_loads_list)
+            finalbeam = beamobjects.BeamFinalPoint(beam=beaminfo, length=beamlength, supports=supports_list, point_loads=point_loads_list)
     else :
         raise ValueError("Invalid number of point loads. Must be greater than 0.")
         
@@ -59,12 +59,12 @@ if numinputs == "distributed" or numinputs == "both":
             x_initial = float(input(f"Enter the initial distance of distributed load {i+1} from the left support in meters: "))
             x_final = float(input(f"Enter the final distance of distributed load {i+1} from the left support in meters: "))
             distributed_load_direction = input(f"Enter the direction of distributed load {i+1} (up or down): ")
-            distributed_load = beamcalc.DistributedLoad(load_type, load_value, x_initial, x_final, distributed_load_direction)
+            distributed_load = beamobjects.DistributedLoad(load_type, load_value, x_initial, x_final, distributed_load_direction)
             distributed_loads_list[i] = distributed_load
         if numinputs == "distributed":
-            finalbeam = beamcalc.BeamFinalDistributed(beam=beaminfo, length=beamlength, supports=supports_list, distributed_loads=distributed_loads_list)
+            finalbeam = beamobjects.BeamFinalDistributed(beam=beaminfo, length=beamlength, supports=supports_list, distributed_loads=distributed_loads_list)
     else:
         raise ValueError("Invalid number of distributed loads. Must be greater than 0.")
 if numinputs == "both":
-    finalbeam = beamcalc.BeamFinalBoth(beam=beaminfo, length=beamlength, supports=supports_list, point_loads=point_loads_list, distributed_loads=distributed_loads_list)
+    finalbeam = beamobjects.BeamFinalBoth(beam=beaminfo, length=beamlength, supports=supports_list, point_loads=point_loads_list, distributed_loads=distributed_loads_list)
     
