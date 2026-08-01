c5e7c80d7ca193bc43e45a8528bfa0ccb06a80e4
SrivanthBReddy <srivanthbojja@gmail.com>
2026-08-01 12:41:06 -0400

    First

---
+++ beamcalc.py
@@ -0,0 +1,132 @@
+from dataclasses import dataclass
+from enum import Enum
+
+#Wood
+class Wood(Enum):
+    YOUNGMODULE = (1.3*(10**10)) 
+    YEILDSTRENGTH = (3*(10**7))
+    DENSITY = ()
+
+#Concrete
+class Concrete(Enum):
+    YOUNGMODULE = ()
+    YEILDSTRENGTH = ()
+    DENSITY = ()
+
+#Steel
+class Steel(Enum):
+    YOUNGMODULE = () 
+    YEILDSTRENGTH = ()
+    DENSITY = () 
+
+#End Supports
+class Support(Enum):
+    FIXED = "Fixed"
+    ROLLER = "Roller"
+    PIN = "Pin"
+    
+#custom
+class beam:
+    def __init__(self, material : str =None,youngs_modulus: float = None, yeild_strength: float = None, density: float = None):
+        if material is not None and youngs_modulus is not None and yeild_strength is not None and density is not None:
+            self.material = material
+            self.youngs_modulus = youngs_modulus
+            self.yeild_strength = yeild_strength
+            self.density = density
+        elif material is None and youngs_modulus is not None and yeild_strength is not None and density is not None:
+            self.material = "Material"
+            self.youngs_modulus = youngs_modulus
+            self.yeild_strength = yeild_strength
+            self.density = density
+        elif str.lower(material) == "wood":
+            self.youngs_modulus = Wood.YOUNGMODULE.value
+            self.yeild_strength = Wood.YEILDSTRENGTH.value
+            self.density = Wood.DENSITY.value
+        elif str.lower(material) == "concrete":
+            self.youngs_modulus = Concrete.YOUNGMODULE.value
+            self.yeild_strength = Concrete.YEILDSTRENGTH.value
+            self.density = Concrete.DENSITY.value
+        elif str.lower(material) == "steel":
+            self.youngs_modulus = Steel.YOUNGMODULE.value
+            self.yeild_strength = Steel.YEILDSTRENGTH.value
+            self.density = Steel.DENSITY.value
+        else : 
+            raise ValueError("Invalid material or missing properties for custom beam. Must be in the order of Mateerial Name, Youngs Moduls, Yeild Strenght, and Density")
+        
+@dataclass
+class SupportObj:
+    type: str
+    distance: float
+
+@dataclass
+class PointLoad:
+    force: float
+    distance: float
+    direction: str
+
+@dataclass
+class DistributedLoad:
+    load_type: str
+    load_value: float
+    x_initial: float
+    x_final: float
+    direction: str
+
+@dataclass
+class BeamFinalPlain:
+    beam: beam
+    length: float
+    supports: list[SupportObj]
+
+@dataclass
+class BeamFinalPoint:
+    beam: beam
+    length: float
+    supports: list[SupportObj]
+    point_loads: list[PointLoad]
+
+@dataclass
+class BeamFinalDistributed:
+    beam: beam
+    length: float
+    supports: list[SupportObj]
+    distributed_loads: list[DistributedLoad]
+
+@dataclass
+class BeamFinalBoth:
+    beam: beam
+    length: float
+    supports: list[SupportObj]
+    point_loads: list[PointLoad]
+    distributed_loads: list[DistributedLoad]
+
+
+#Preset or Custom
+
+#if Preset
+#Wood Concrete or Steel
+#What is the length of your beam in meters?
+#how many supports?
+#state the support type, where in meters from the left support in the form, and the orientation of the support(top or bottom) (Support Type, Distance from left support, orientation), ...
+#(optional)State the load applied to the beam and where in Newtons and meters in the form (Force, Distance from left support), ...
+#(optional)State the distritubted load applied type(rectangular, triangluar bottom to top, triangular top to bottom) and the load in Newtons per meter and the start and end distance from the left support in meters in the form (Load Type, Load in N/m, x initial from left support, x final from left support), ...
+
+
+#if custom
+#Enter your material name, Youngs Modulus, Yeild Strength, and Density in that order.
+#What is the length of your beam in meters?
+#how many supports?
+#state the support type and where in meters from the left support in the form (Support Type, Distance from left support), ...
+#(optional)State the load applied to the beam and where in Newtons and meters in the form (Force, Distance from left support), ...
+#(optional)State the distritubted load applied type(rectangular, triangluar bottom to top, triangular top to bottom) and the load in Newtons per meter and the start and end distance from the left support in meters in the form (Load Type, Load in N/m, x initial from left support, x final from left support), ...
+
+#output should be a graph of the force, moment, deflection, stress, and strain of the beam of the beam with the maximum values of each displayed on the graph.
+
+
+
+
+
+
+
+
+
---
+++ beamquestions.py
@@ -0,0 +1,70 @@
+import beamcalc
+
+beam_type = input("Preset or Custom? ")
+
+if beam_type.lower() == "preset":
+    material = input("Wood, Concrete, or Steel?").lower()
+    beaminfo = beamcalc.beam(material)
+    beamlength = float(input("What is the length of your beam in meters? "))
+elif beam_type.lower() == "custom":
+    name = input("Enter your material name: ")
+    youngs_modulus = float(input("Enter Young's Modulus in Pascals: "))
+    yield_strength = float(input("Enter Yield Strength in Pascals: "))
+    density = float(input("Enter Density in kg/m^3: "))
+    beaminfo = beamcalc.beam(material=name, youngs_modulus=youngs_modulus, yeild_strength=yield_strength, density=density)
+    beamlength = float(input("What is the length of your beam in meters? "))
+else:
+    raise ValueError("Invalid beam type. Please enter 'Preset' or 'Custom'.")
+
+supports = int(input("How many supports?"))
+if supports > 0:
+    supports_list = [None]*supports
+    for i in range(supports):
+        support_type = input(f"Enter the type of support {i+1}: ")
+        distance = float(input(f"Enter the distance of support {i+1} from the left support: "))
+        support = beamcalc.SupportObj(support_type, distance)
+        supports_list[i] = support
+else:
+    raise ValueError("Invalid number of supports. Must be greater than 0.")
+
+numinputs = input("input point, distributed, both, or none: ")
+numpinputs = numinputs.lower()
+
+#Optional Input for point loads
+if numinputs == "none":
+    finalbeam = beamcalc.BeamFinalPlain(beam=beaminfo, length=beamlength, supports=supports_list)   
+if numinputs == "point" or numinputs == "both":
+    loads = int(input("How many point loads? "))
+    if loads > 0:
+        point_loads_list = [None]*loads
+        for i in range(loads):
+            force = float(input(f"Enter the force of load {i+1} in Newtons: "))
+            distance = float(input(f"Enter the distance of load {i+1} from the left support in meters: "))
+            point_load_direction = input(f"Enter the direction of load {i+1} (up or down): ")
+            point_load = beamcalc.PointLoad(force, distance, point_load_direction)  
+            point_loads_list[i] = point_load
+        if numinputs == "point":
+            finalbeam = beamcalc.BeamFinalPoint(beam=beaminfo, length=beamlength, supports=supports_list, point_loads=point_loads_list)
+    else :
+        raise ValueError("Invalid number of point loads. Must be greater than 0.")
+        
+#Optional input for distributed loads
+if numinputs == "distributed" or numinputs == "both":
+    distributed_loads = int(input("How many distributed loads? "))
+    if distributed_loads > 0:
+        distributed_loads_list = [None]*distributed_loads
+        for i in range(distributed_loads):
+            load_type = input(f"Enter the type of distributed load {i+1} (rectangular, triangular bottom to top, triangular top to bottom): ")
+            load_value = float(input(f"Enter the load value of distributed load {i+1} in N/m(if triangular state the largest value of load): "))
+            x_initial = float(input(f"Enter the initial distance of distributed load {i+1} from the left support in meters: "))
+            x_final = float(input(f"Enter the final distance of distributed load {i+1} from the left support in meters: "))
+            distributed_load_direction = input(f"Enter the direction of distributed load {i+1} (up or down): ")
+            distributed_load = beamcalc.DistributedLoad(load_type, load_value, x_initial, x_final, distributed_load_direction)
+            distributed_loads_list[i] = distributed_load
+        if numinputs == "distributed":
+            finalbeam = beamcalc.BeamFinalDistributed(beam=beaminfo, length=beamlength, supports=supports_list, distributed_loads=distributed_loads_list)
+    else:
+        raise ValueError("Invalid number of distributed loads. Must be greater than 0.")
+if numinputs == "both":
+    finalbeam = beamcalc.BeamFinalBoth(beam=beaminfo, length=beamlength, supports=supports_list, point_loads=point_loads_list, distributed_loads=distributed_loads_list)
+    
