03f71be690143e644ef1bb8c5f5966a5db4c1182
SrivanthBReddy <srivanthbojja@gmail.com>
2026-08-09 17:35:37 -0400

    Add the barebones calculation for the global stifness matrix. Stil have to caluculate the variable degrees of freedom as well as the actual nodes generated from the user

--- beamcalcs.py
+++ beamcalcs.py
@@ -3,6 +3,13 @@ import numpy as np
 import beamobjects
 from beamquestions import finalbeam
 
+"""
+How does the stifness matrix work?
+Each element in the stifness matrix is a representative of the reactions that occur to applied forces/moments
+The process proceeds by having all the local stifness matrix's identified based on the meshes created
+Lets say you have N nodes [1, .., N]. Your system size is going to be 2 x (N+1) w 2 DOFs (v, theta) per node
+The elemnts in your calculation is going to be N-1 with 
+"""
 
 E = 200* 10**9 #PA
 I = 1*10**-4 #m^4 
@@ -15,10 +22,26 @@ L = finalbeam.length # Length of the beam in meters
 S = finalbeam.supports # List of supports
 P = finalbeam.point_loads # List of point loads
 
-stiffmatrix = ((E*I)/L**3) * np.array([[12, 6*L, -12, 6*L], 
+x = [0, 3, 7] # Example of the node positions used for calculations
+
+
+node_elements = len(x) - 1 # reads as 2
+matrix_list = [None] * node_elements
+for i in range(node_elements): # reads as 0, 1
+    L = x[i+1] - x[i]
+    matrix_list[i] = ((E*I)/L**3) * np.array([[12, 6*L, -12, 6*L], 
                                        [6*L, 4*L**2, -6*L, 2*L**2], 
                                        [-12, -6*L, 12, -6*L], 
                                       [6*L, 2*L**2, -6*L, 4*L**2]])
-stiffmatrix = stiffmatrix[2:4, 2:4]
-solved = np.linalg.solve(stiffmatrix, np.array([[P],[0]]))
-print(solved)
+
+print(matrix_list[0])
+print(matrix_list[1])
+
+# construct global stiffness matrix
+
+Global_matrix = np.zeros((2*(len(x)), 2*(len(x))))
+
+for i in range(node_elements):
+    Global_matrix[2*i:2*i+ 4, 2*i:2*i + 4] += matrix_list[i]
+
+print(Global_matrix[2:4, 2:4])
